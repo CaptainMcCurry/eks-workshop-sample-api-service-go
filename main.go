c759db18df905b851eb5d7f32c05b3cc46131323
CaptainMcCurry <42413487+CaptainMcCurry@users.noreply.github.com>
2019-12-02 20:43:57 -0800

    updates yo

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World Yo"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
